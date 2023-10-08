af179a5511e6841772ee25e5a22a3f8388e3f5e3
Jessica Lopes <jessicalopes.dev@gmail.com>
2023-10-08 11:09:40 -0300

    fix: checklist

--- src/main/java/com/betrybe/museumfinder/dto/MuseumDto.java
+++ src/main/java/com/betrybe/museumfinder/dto/MuseumDto.java
@@ -6,12 +6,12 @@ import com.betrybe.museumfinder.model.Coordinate;
  * MuseumDto.
  */
 public record MuseumDto(
-  Long id,
-  String name,
-  String description,
-  String address,
-  String collectionType,
-  String subject,
-  String url,
-  Coordinate coordinate
+    Long id,
+    String name,
+    String description,
+    String address,
+    String collectionType,
+    String subject,
+    String url,
+    Coordinate coordinate
 ) {}
